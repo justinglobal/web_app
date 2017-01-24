d280f2bbcd70d3185846576acd1bd97388c5445f
justin <justinglobal@gmail.com>
2017-01-24 13:33:54 -0800

    fixed enviro variables

--- asciibot.py
+++ asciibot.py
@@ -1,20 +1,26 @@
 # This is a twitter bot
 
 # import secret keys
-from keys import my_secret
+# from keys import my_secret
+import os
 
 # Importing the module
 from twython import Twython
 
 # change these when app is running on heroku w/ environmental variables
-app_key = my_secret.app_key
-app_secret = my_secret.app_secret
-oauth_token = my_secret.oauth_token
-oauth_token_secret = my_secret.oauth_token_secret
+# app_key = my_secret.app_key
+# app_secret = my_secret.app_secret
+# oauth_token = my_secret.oauth_token
+# oauth_token_secret = my_secret.oauth_token_secret
+
+app_key = os.environ['APP_KEY']
+app_secret = os.environ['APP_SECRET']
+oauth_token = os.environ['OAUTH_TOKEN']
+oauth_token_secret = os.environ['OAUTH_TOKEN_SECRET']
 
 # Prepare your twitter, you will need it for everything
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 # The above should just be a single line, without the break
 
 # The obligatory first status update to test
-twitter.update_status(status=chr(128123) + "Follow JustinCybertron on Instagram for more digital art!" + chr(128123))
+twitter.update_status(status=chr(128123) + 'Maximum Overdrive' + chr(128123))
