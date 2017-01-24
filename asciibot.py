d5b6327644b1f617c343dcbc91f611cefb8c6a7c
justin <justinglobal@gmail.com>
2017-01-23 19:18:29 -0800

    asciibot experiments

--- asciibot.py
+++ asciibot.py
@@ -6,7 +6,7 @@ from keys import my_secret
 # Importing the module
 from twython import Twython
 
-
+# change these when app is running on heroku w/ environmental variables
 app_key = my_secret.app_key
 app_secret = my_secret.app_secret
 oauth_token = my_secret.oauth_token
@@ -17,4 +17,4 @@ twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 # The above should just be a single line, without the break
 
 # The obligatory first status update to test
-twitter.update_status(status=chr(128125) + "Intergalactic Purifier" + chr(128125))
+twitter.update_status(status=chr(128123) + "Follow JustinCybertron on Instagram for more digital art!" + chr(128123))
