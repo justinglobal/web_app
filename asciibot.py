1f2af3533b0675b9a7ec3233e368f25b972a2e46
justin <justinglobal@gmail.com>
2017-02-13 19:03:50 -0800

    update with b64 experiments

--- asciibot.py
+++ asciibot.py
@@ -3,7 +3,10 @@
 # import secret keys
 # from keys import my_secret
 import os
+import urllib.request
+import base64
 
+from imageserv import image02
 # Importing the module
 from twython import Twython
 
@@ -19,12 +22,52 @@ oauth_token = os.environ['OAUTH_TOKEN']
 oauth_token_secret = os.environ['OAUTH_TOKEN_SECRET']
 
 # Prepare your twitter, you will need it for everything
-twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+# twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 # The above should just be a single line, without the break
+
+# with urllib.request.urlopen('https://shirtfactory.herokuapp.com/design/image/37') as img_raw:
+    # print(type(img_raw))
+
+
+## get bytecode from shirtfactory.heroku
+with urllib.request.urlopen('https://shirtfactory.herokuapp.com/design/image/37') as img_raw:
+    img_bytes = img_raw.read()
+
+img_d64 = base64.b64decode(img_bytes)
+
+print(type(img_d64))
+img_d64_str = str(img_d64)
+print(img_d64_str[0:30])
+print('___________________')
+
+## convert img_code from bytes to string
+img_str = str(img_bytes)
+
+print(type(img_str))
+print(img_str[0:30])
+
+print('______________________________')
+
+print(type(image02))
+print(image02[0:30])
+# check if img_code needs to be a list
 
 # def makemsg(message):
 #     """function to make message?"""
 #     return
 
 # The obligatory first status update to test
-twitter.update_status(status=chr(128126) + 'Heroku check 2' + chr(128126))
+# twitter.update_status(status=chr(128079) + 'Heroku check 7' + chr(128079))
+
+# twitter.update_status(status=image01)
+
+# img = twitter.request('https://shirtfactory.herokuapp.com/design/image/46').read()
+# response = twitter.upload_media(media=img)
+# response = twitter.upload_media(media_data=img)
+
+# this is current code below:
+# response = twitter.upload_media(media=img_str)
+# #
+# twitter.update_status(status='image check 04', media_ids=[response['media_id']])
+
+# response = twitter.upload_media(media_data='https://shirtfactory.herokuapp.com/design/image/46')
